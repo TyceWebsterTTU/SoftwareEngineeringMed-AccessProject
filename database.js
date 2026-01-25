de9c0084f0d86e761fb3fcb820ee55f1c6a53538
Connor Buttrey <cbuttrey0216@gmail.com>
2026-01-25 13:47:11 -0600

    Create database.js
    Created database.js with connection data for the database

---
+++ database.js
@@ -0,0 +1,9 @@
+// Connection Data for the database
+const objConnectionData = {
+    host: 'localhost',
+    user: 'root',
+    port: 3306,
+    password: "Admin123!",
+    database: "dbMedAccess"
+}
+
